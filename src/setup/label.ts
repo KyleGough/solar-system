57f2feaed24de45c7fd2dbc4795099e30ffab3c5
Kyle Gough <kylegough98@gmail.com>
2023-07-29 00:06:41 +0100

    Fix label icons

--- src/setup/label.ts
+++ src/setup/label.ts
@@ -64,7 +64,7 @@ export const createLabel = (
 
   if (type) {
     const img = document.createElement("img");
-    img.src = `/icons/${type}.svg`;
+    img.src = `./icons/${type}.svg`;
     container.appendChild(img);
   }
 
